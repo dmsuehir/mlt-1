c93099a9d4729dbe856e5ec8d36d377c433487a9
Abolfazl Shahbazi <abolfazl.shahbazi@intel.com>
2018-04-06 14:03:13 -0700

    Fix tests

--- tests/unit/commands/test_templates.py
+++ tests/unit/commands/test_templates.py
@@ -27,36 +27,41 @@ from test_utils import project
 from test_utils.io import catch_stdout
 
 
-@pytest.mark.parametrize("template_dir", [
+@pytest.mark.parametrize("valid_template_dir", [
     project.basedir(),
     "git@github.com:IntelAI/mlt.git",
     "https://github.com/IntelAI/mlt",
-    "git@github.com:1ntelA1/mlt.git",
 ])
-
-def test_template_list(template_dir):
+def test_template_list(valid_template_dir):
     args = {
         'template': 'test',
         'list': True,
-        '--template-repo': template_dir
+        '--template-repo': valid_template_dir
     }
     templates = TemplatesCommand(args)
     with catch_stdout() as caught_output:
         templates.action()
         assert caught_output.getvalue() is not None
 
-def test_template_list_invalid_repo_dir():
-    invalid_repo_dir = "/tmp/invalid-mlt-dir"
+@pytest.mark.parametrize("invalid_template_dir", [
+    "/tmp/invalid-mlt-dir",
+    "git@github.com:1ntelA1/mlt.git",
+    "https://github.com/1ntelA1/mlt",
+])
+def test_template_list_invalid_repo_dir(invalid_template_dir):
+    invalid_template_dir = "/tmp/invalid-mlt-dir"
     args = {
         'template': 'test',
         'list': True,
-        '--template-repo': invalid_repo_dir
+        '--template-repo': invalid_template_dir
     }
 
-    if os.path.exists(invalid_repo_dir):
-        shutil.rmtree(invalid_repo_dir)
+    if invalid_template_dir.startswith("/tmp/"):
+        if os.path.exists(invalid_template_dir):
+            shutil.rmtree(invalid_template_dir)
 
     templates = TemplatesCommand(args)
     with catch_stdout() as caught_output:
         templates.action()
         assert caught_output.getvalue() is not None
+
